c11a113211e38a3517d11f81dab91da56562ec0a
patricia cabrero <patrcabr@ucm.es>
2019-05-09 17:57:27 +0200

    Inicio

---
+++ ProfilerExterno/ProfilerExterno/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ProfilerExterno
+{
+    class Program
+    {
+        private static PerformanceCounter cpuCounter;
+        private static PerformanceCounter ramCounter;
+        static void Main(string[] args)
+        {
+            
+            initialize("hola");
+            do
+            {
+                using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter("WriteLines2.txt", true))
+                {
+                    file.WriteLine(getCurrentCpuUsage() + ";" + getAvailableRAM());
+                    file.Close();
+                }
+                System.Threading.Thread.Sleep(500);
+            } while (true);
+        }
+     
+
+        public static void initialize(string Proceso)
+        {
+
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", Proceso);
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+        }
+
+        public static string getCurrentCpuUsage()
+        {
+            return cpuCounter.NextValue().ToString();
+        }
+
+        public static string getAvailableRAM()
+        {
+            return ramCounter.NextValue().ToString();
+        }
+    }
+}
---
+++ Proyecto-Grupo03/Assets/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Diagnostics;
+
+public class Program : MonoBehaviour {
+
+    // Use this for initialization
+    Process myProcess;
+        void Start () {
+        try
+        {
+            String[] Data = Environment.GetCommandLineArgs();
+            if (Data[1] == "Profiler")
+            {
+                using (System.IO.StreamWriter file =
+               new System.IO.StreamWriter("Profiler.txt", false))
+                {
+                    file.WriteLine(Application.productName);
+                }
+            }
+            myProcess = new Process();
+            myProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            myProcess.StartInfo.CreateNoWindow = true;
+            myProcess.StartInfo.UseShellExecute = false;
+            myProcess.StartInfo.FileName = Application.streamingAssetsPath + "/ProfilerExterno.exe";
+            myProcess.EnableRaisingEvents = true;
+            myProcess.Start();
+            
+            
+            //myProcess.WaitForExit();
+            //int ExitCode = myProcess.ExitCode;
+            //print(ExitCode);
+        }
+        catch (Exception e)
+        {
+            print(e);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnApplicationQuit()
+    {
+        UnityEngine.Debug.Log("HOLA");
+        myProcess.Kill();
+        //myProcess.Close();
+    }
+}
