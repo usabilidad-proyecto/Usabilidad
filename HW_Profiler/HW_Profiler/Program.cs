6eb9bc8649fe7ff40ff1469bd80d6483636ab03c
Patricia Cabrero <patrcabr@ucm.es>
2019-05-15 21:26:49 +0200

    Añadido parametro uso de ram del proceso y mejorado los scripts de hardware

--- HW_Profiler/HW_Profiler/Program.cs
+++ HW_Profiler/HW_Profiler/Program.cs
@@ -1,55 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics;
 
-namespace ProfilerExterno
+
+namespace HW_Profiler
 {
     class Program
     {
         private static PerformanceCounter cpuCounter;
+        private static PerformanceCounter ramProcessCounter;
         private static PerformanceCounter ramCounter;
         private static string proceso;
+
+        ///Recibe el nombre del proceso a medir y como segundo parametro el tiempo en segundos(UNIX TIMESTAMP)
+        ///el tiempo nos permitirá crear un archivo único para cada ejecución ligado al profiler interno.
         static void Main(string[] args)
         {
             //Floats con puntos en vez de comas
             System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
             customCulture.NumberFormat.NumberDecimalSeparator = ".";
-
             System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
 
             proceso = args[0];
             //Mirar si existe el proceso
             initialize(proceso);
-
-            do
+            using (System.IO.StreamWriter file =
+               new System.IO.StreamWriter(args[1] + "-HWProfiler.txt", true))
             {
-                using (System.IO.StreamWriter file =
-                new System.IO.StreamWriter(proceso+"-profiler.txt", true))
+                do
                 {
-                    file.WriteLine(getCurrentCpuUsage()+  ";" + getAvailableRAM());
-                    file.Close();
-                }
-                System.Threading.Thread.Sleep(500);
-                
-            } while (getProcess());
+                    file.WriteLine(DateTime.Now.ToString("yyyyMMddHHmmssffff") + ";" + getRamProcess() + ";" + getCurrentCpuUsage() + ";" + getAvailableRAM());
+                    System.Threading.Thread.Sleep(500);
+                } while (getProcess());
+                file.Close();
+            }
+            close();
+            
         }
-        private static bool getProcess() {
+        private static bool getProcess()
+        {
             Process[] a = Process.GetProcessesByName(proceso);
             return a.GetLength(0) != 0;
         }
 
         public static void initialize(string Proceso)
         {
-            try            
+            try
             {
-                
                 cpuCounter = new PerformanceCounter("Process", "% Processor Time", Proceso);
                 ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+                ramProcessCounter = new PerformanceCounter("Process", "Working Set - Private", Proceso);
             }
-            catch {
+            catch
+            {
                 using (System.IO.StreamWriter file =
                new System.IO.StreamWriter(proceso + "DEBUG.txt", true))
                 {
@@ -58,25 +60,50 @@ namespace ProfilerExterno
                 }
             }
         }
+        public static void close() {
+            cpuCounter.Close();
+            cpuCounter.Dispose();
+            ramCounter.Close();
+            ramCounter.Dispose();
+            ramProcessCounter.Close();
+            ramProcessCounter.Dispose();
+        }
 
+        //USO DE LA CPU EN PORCENTAJE
         public static string getCurrentCpuUsage()
         {
             try
             {
                 return cpuCounter.NextValue().ToString();
             }
-            catch {
+            catch
+            {
                 return "";
             }
         }
 
+        //RAM DISPONIBLE (GENERAL)
         public static string getAvailableRAM()
         {
             try
             {
                 return ramCounter.NextValue().ToString();
             }
-            catch {
+            catch
+            {
+                return "";
+            }
+        }
+
+        ///Uso en kb de memoria ram del proceso
+        public static string getRamProcess()
+        {
+            try
+            {
+                return ((int)ramProcessCounter.NextValue()>>10).ToString();
+            }
+            catch
+            {
                 return "";
             }
         }
---
+++ Proyecto-Grupo03/Assets/rotaCube.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotaCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.RotateAroundLocal(Vector3.up, 0.1f);
+   
+	}
+}
